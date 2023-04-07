a15a389a26e0eaf237a7f82ced9c7d86e9e8d6e0
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-04-07 11:09:16 +0700

    menambahkan integer

--- easd_1_064_B/easd_1_064_B.cpp
+++ easd_1_064_B/easd_1_064_B.cpp
@@ -49,6 +49,11 @@ void mergeSort(int low, int high) {
     if (low >= high) {
         return;
     }
+    int mid = (low + high) / 2;
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+    merge(low, mid, high);
+}
 
 
 
