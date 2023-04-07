a7f475a5d75ed5db4f79df549df1ceb4090b3261
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-04-07 10:17:13 +0700

    Add project files.

---
+++ easd_1_064_B/easd_1_064_B.cpp
@@ -0,0 +1,5 @@
+//karena algoritma dapat memecahkan masalah secara sistematis dan bisa mengoptimalkan waktu dalam memecakan masalah 
+//array dan link list
+//sepeed of the machine, complier, operating system, programming language, size of the input
+//quicksort
+//bubble sort, insertion sort, selesction sort merupakan (quadratic), Quick sort, marge sort, shell sort (logliner)
